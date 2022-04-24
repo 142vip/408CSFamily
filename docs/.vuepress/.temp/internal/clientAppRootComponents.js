f5daee2ae39a69aeb8d2630b7f17ce116243b1c3
mmdapl <2237221210@qq.com>
2022-04-24 09:08:22 +0800

    feat: 修复 docs

--- docs/.vuepress/.temp/internal/clientAppRootComponents.js
+++
@@ -1,5 +0,0 @@
-import clientAppRootComponent0 from '/Users/mmdapl/Desktop/github/408CSFamily/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
-
-export const clientAppRootComponents = [
-  clientAppRootComponent0,
-]

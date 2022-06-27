bac72ffaa554547de1973988262949725fa2c77e
喜欢芝士的妹妹 <fairy0115@2925.com>
2022-06-27 09:35:54 +0800

    fix: 部署脚本修改

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -2,8 +2,8 @@
 ## 功能：本地前后端项目构建、打包镜像，上传docker仓库
 ## 参考：https://blog.csdn.net/Dontla/article/details/125210694
 ## 作者：Rong姐姐好可爱
-## 使用示例：bash xxx.sh 容器名称  版本号
-##    -  bash build_image.sh oauth_login 0.0.1
+## 使用示例：bash xxx.sh  版本号
+##    -  bash build_image.sh  0.0.1
 ##
 
 

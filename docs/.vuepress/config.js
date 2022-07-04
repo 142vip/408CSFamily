c420e7fd588fa8dcbc6c9832582ac50829f73261
妹妹下雨回不去 <fairy_408@2925.com>
2022-07-04 17:37:59 +0800

    feat: 配置百度统计

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,7 +16,19 @@ module.exports = {
   // base: "/408CSFamily/",
   port: 4200,
   head: [
-    ["link", { rel: "icon", href: "/408_favicon.ico" }]
+    [
+        "link", { rel: "icon", href: "/408_favicon.ico" }
+    ],
+    [// 百度统计
+      'script',{},`
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?3515cc46ae60747b778140f0e5e22dfe";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+      })();`
+    ]
   ],
   themeConfig: {
     logo: "/assets/408_logo.png",

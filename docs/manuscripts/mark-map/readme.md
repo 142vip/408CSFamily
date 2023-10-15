b68ade5005e89861172c9be1b0e5a9b5a741e464
chu fan <fairy_408@2925.com>
2023-10-15 21:27:34 +0800

    fix: 修复思维导图显示链接跳转异常

--- docs/manuscripts/mark-map/readme.md
+++ docs/manuscripts/mark-map/readme.md
@@ -2,10 +2,10 @@
 
 > 思维导图汇总整理，内容更新慢于笔记，赶稿中...
 
-- <a href="../../mark-map/ds-map.html" target="_blank">数据结构</a>
-- <a href="../../mark-map/os-map.html" target="_blank">操作系统</a>
-- <a href="../../mark-map/ccp-map.html" target="_blank">计算机组成原理</a>
-- <a href="../../mark-map/cn-map.html" target="_blank">计算机网络</a>
+- <a href="../mark-map/ds-map.html" target="_blank">数据结构</a>
+- <a href="../mark-map/os-map.html" target="_blank">操作系统</a>
+- <a href="../mark-map/ccp-map.html" target="_blank">计算机组成原理</a>
+- <a href="../mark-map/cn-map.html" target="_blank">计算机网络</a>
 
 
 [//]: # (xmind在线加载时间太长)
--- docs/manuscripts/os/os.sidebar.ts
+++ docs/manuscripts/os/os.sidebar.ts
@@ -21,98 +21,98 @@ export const osSidebar = [
         link: '4.体系结构.md'
       }
     ]
-  },
-  {
-    text: '进程管理',
-    prefix: '进程管理',
-    collapsible: true,
-    children: [
-      {
-        text: '进程和线程',
-        link: '1.进程和线程.md'
-      },
-      {
-        text: '处理机调度',
-        link: '2.处理机调度.md'
-      },
-      {
-        text: '进程同步',
-        link: '3.进程同步.md'
-      },
-      {
-        text: '死锁',
-        link: '4.死锁.md'
-      },
-      {
-        text: '一些总结',
-        link: '5.总结.md'
-      }
-    ]
-  },
-  {
-    text: '内存管理',
-    prefix: '内存管理',
-    collapsible: true,
-    children: [
-      {
-        text: '引论',
-        link: '1.引论.md'
-      },
-      {
-        text: '虚拟内存',
-        link: '2.虚拟内存.md'
-      },
-      {
-        text: '一些总结',
-        link: '3.总结.md'
-      }
-    ]
-  },
-  {
-    text: '文件管理',
-    prefix: '文件管理',
-    collapsible: true,
-    children: [
-      {
-        text: '基本概念',
-        link: '1.基本概念.md'
-      },
-      {
-        text: '文件系统',
-        link: '2.文件系统.md'
-      },
-      {
-        text: '磁盘管理',
-        link: '3.磁盘管理.md'
-      },
-      {
-        text: '一些总结',
-        link: '4.总结.md'
-      }
-
-    ]
-  },
-  {
-    text: '输入、输出管理',
-    prefix: '输入、输出管理',
-    collapsible: true,
-    children: [
-      {
-        text: '基本概述',
-        link: '1.基本概述g.md'
-      },
-      {
-        text: '核心子系统',
-        link: '2.核心子系统.md'
-      },
-      {
-        text: '高速缓存和缓冲区',
-        link: '3.高速缓存和缓冲区.md'
-      },
-      {
-        text: '一些总结',
-        link: '4.总结.md'
-      }
-    ]
   }
+  // {
+  //   text: '进程管理',
+  //   prefix: '进程管理',
+  //   collapsible: true,
+  //   children: [
+  //     {
+  //       text: '进程和线程',
+  //       link: '1.进程和线程.md'
+  //     },
+  //     {
+  //       text: '处理机调度',
+  //       link: '2.处理机调度.md'
+  //     },
+  //     {
+  //       text: '进程同步',
+  //       link: '3.进程同步.md'
+  //     },
+  //     {
+  //       text: '死锁',
+  //       link: '4.死锁.md'
+  //     },
+  //     {
+  //       text: '一些总结',
+  //       link: '5.总结.md'
+  //     }
+  //   ]
+  // },
+  // {
+  //   text: '内存管理',
+  //   prefix: '内存管理',
+  //   collapsible: true,
+  //   children: [
+  //     {
+  //       text: '引论',
+  //       link: '1.引论.md'
+  //     },
+  //     {
+  //       text: '虚拟内存',
+  //       link: '2.虚拟内存.md'
+  //     },
+  //     {
+  //       text: '一些总结',
+  //       link: '3.总结.md'
+  //     }
+  //   ]
+  // },
+  // {
+  //   text: '文件管理',
+  //   prefix: '文件管理',
+  //   collapsible: true,
+  //   children: [
+  //     {
+  //       text: '基本概念',
+  //       link: '1.基本概念.md'
+  //     },
+  //     {
+  //       text: '文件系统',
+  //       link: '2.文件系统.md'
+  //     },
+  //     {
+  //       text: '磁盘管理',
+  //       link: '3.磁盘管理.md'
+  //     },
+  //     {
+  //       text: '一些总结',
+  //       link: '4.总结.md'
+  //     }
+  //
+  //   ]
+  // },
+  // {
+  //   text: '输入、输出管理',
+  //   prefix: '输入、输出管理',
+  //   collapsible: true,
+  //   children: [
+  //     {
+  //       text: '基本概述',
+  //       link: '1.基本概述g.md'
+  //     },
+  //     {
+  //       text: '核心子系统',
+  //       link: '2.核心子系统.md'
+  //     },
+  //     {
+  //       text: '高速缓存和缓冲区',
+  //       link: '3.高速缓存和缓冲区.md'
+  //     },
+  //     {
+  //       text: '一些总结',
+  //       link: '4.总结.md'
+  //     }
+  //   ]
+  // }
 ]

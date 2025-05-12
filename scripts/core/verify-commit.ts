8d604a02429576ca815feb87b7729ac439fa09cc
chufan <mmdapl@163.com>
2025-05-12 19:03:30 +0800

    feat: 引入`@142vip/commit-linter`依赖，增加`check:commit`命令，校验`commit`信息

---
+++ scripts/core/verify-commit.ts
@@ -0,0 +1,15 @@
+import { commitLiner } from '@142vip/commit-linter'
+import { VipColor, VipConsole, vipLogger } from '@142vip/utils'
+
+/**
+ * 验证Git Commit信息
+ */
+async function verifyCommitMain(): Promise<void> {
+  const { type, scope, subject, commit } = commitLiner()
+
+  // 提交符合规范，打印相关信息
+  VipConsole.log(`type: ${type}, scope: ${scope}, subject: ${subject}`)
+  vipLogger.logByBlank(`${VipColor.greenBright('Git Commit: ')} ${VipColor.green(commit)}`)
+}
+
+void verifyCommitMain()
